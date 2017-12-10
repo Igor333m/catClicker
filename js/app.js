ade72d645f7e7d18d127587e09880f3a0b7d1222
Igor Todorovic <igor333m@yahoo.com>
2017-12-09 18:38:35 -0800

    bug: Fix num of clicks

--- js/app.js
+++ js/app.js
@@ -1,15 +1,43 @@
-let click1 = 0;
-let click2 = 0;
-let cats = ["Miki", "Spasoje"];
+let cats = [
+	{
+		name: "Miki",
+		img: "cat.jpg",
+		numClicks: 0
+	},
+	{
+		name: "Spasoje",
+		img: "cat2.jpg",
+		numClicks:0
+	}
+];
 
-$("h1").each( function (index) {
-	console.log(cats[index]);
+let containerHtml = '';
+// Displays all cat names from cats array
+for(let i = 0; i < cats.length; i++) {
+	containerHtml += `
+		<article>
+			<h1>${cats[i].name}</h1>
+			<img src="img/${cats[i].img}" alt="${cats[i].name} the cat" data-cat="${cats[i].name}">
+        	<h2>Number of clicks <span>${cats[i].numClicks}</span></h2>
+        </article>
+		`;
+}
+
+$(".container").append(containerHtml);
+
+/*cats.foreach( function (index) {
+	console.log(cats[index].name);
 	console.log($(this));
-	$(this).text(cats[index]);
-});
+	$(this).text(cats[index].name);
+});*/
 
-$("img").click( () => {
-		$("h2 span").html(clicks);
-		console.log("click");
+$("img").click( function() {
+		for(let i = 0; i < cats.length; i++) {
+			if($(this).attr("data-cat") === cats[i].name) {
+				cats[i].numClicks++;
+				$(this).next().find("span").text(cats[i].numClicks);
+
+			}
+		}
 	}
 );
