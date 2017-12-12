9ee3d7b204b9d4d112966cdd96126ba671111e26
Igor Todorovic <igor333m@yahoo.com>
2017-12-12 08:39:28 -0800

    feat: New menu

--- js/app.js
+++ js/app.js
@@ -13,20 +13,53 @@ let cats = [
 		name: "Ana",
 		img: "cat3.jpg",
 		numClicks: 0
+	},
+	{
+		name: "Arthur",
+		img: "cat4.jpg",
+		numClicks: 0
+	},
+	{
+		name: "Aragorn",
+		img: "cat5.jpg",
+		numClicks: 0
+	},
+	{
+		name: "Lilly",
+		img: "cat6.jpg",
+		numClicks: 0
 	}
 ];
 
+let listHtml = "";
 let containerHtml = '';
-// Displays all cat names from cats array
 for(let i = 0; i < cats.length; i++) {
-	containerHtml += `
-		<article>
-			<h1>${cats[i].name}</h1>
-			<img src="img/${cats[i].img}" alt="${cats[i].name} the cat" data-cat="${cats[i].name}">
-        	<h2>Number of clicks <span>${cats[i].numClicks}</span></h2>
-        </article>
-		`;
+
+	// Displays cats names
+	listHtml += `
+		<ul>
+			<li>${cats[i].name}</li>	
+		</ul>
+	`;
 }
+$(".list").append(listHtml);
+
+$("ul").click(function() {
+	console.log($(this).children("li").text());
+	for(let i = 0; i < cats.length; i++) {
+		if( $(this).children("li").text() === cats[i].name   ) {
+			console.log($(this).children("li").text());
+			// Displays all cat names, images and num of clicks from cats array
+			containerHtml = `
+				<article>
+					<h1>${cats[i].name}</h1>
+					<img src="img/${cats[i].img}" alt="${cats[i].name} the cat" data-cat="${cats[i].name}">
+		        	<h2>Number of clicks <span>${cats[i].numClicks}</span></h2>
+		        </article>
+		`;
+		}
+	}
+});
 
 $(".container").append(containerHtml);
 
