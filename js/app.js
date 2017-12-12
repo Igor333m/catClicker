cca41e04b8bce3c35dfc8c3fc6813269953ae6cb
Igor Todorovic <igor333m@yahoo.com>
2017-12-12 11:05:08 -0800

    bug: img bug fix

--- js/app.js
+++ js/app.js
@@ -31,34 +31,41 @@ let cats = [
 	}
 ];
 
-let listHtml = "";
+let listHtml = $(".list");
 let containerHtml = '';
 for(let i = 0; i < cats.length; i++) {
 
 	// Displays cats names
-	listHtml += `
-		<ul>
-			<li>${cats[i].name}</li>	
-		</ul>
-	`;
-}
-$(".list").append(listHtml);
+	listHtml.append(`<li>${cats[i].name}</li>`);
 
-$("ul").click(function() {
-	console.log($(this).children("li").text());
-	for(let i = 0; i < cats.length; i++) {
-		if( $(this).children("li").text() === cats[i].name   ) {
-			console.log($(this).children("li").text());
-			// Displays all cat names, images and num of clicks from cats array
-			containerHtml = `
-				<article>
-					<h1>${cats[i].name}</h1>
-					<img src="img/${cats[i].img}" alt="${cats[i].name} the cat" data-cat="${cats[i].name}">
-		        	<h2>Number of clicks <span>${cats[i].numClicks}</span></h2>
-		        </article>
-		`;
+	$("li").click(function() {
+		console.log($(this).text());
+
+			if( $(this).text() === cats[i].name ) {
+				console.log("if!");
+				// Displays all cat names, images and num of clicks from cats array
+					containerHtml = `
+					<article>
+						<h1>${cats[i].name}</h1>
+						<img src="img/${cats[i].img}" alt="${cats[i].name} the cat" data-cat="${cats[i].name}">
+			        	<h2>Number of clicks <span>${cats[i].numClicks}</span></h2>
+			        </article>
+			`;
+			$(".container").html(containerHtml);
 		}
-	}
+	});
+	$(".list").append(listHtml);
+}
+
+$(".container").on("click", "img", function() {
+		console.log("IMG");
+		for(let i = 0; i < cats.length; i++) {
+			if($(this).attr("data-cat") === cats[i].name) {
+				cats[i].numClicks++;
+				$(this).next().find("span").text(cats[i].numClicks);
+
+			}
+		}
 });
 
 $(".container").append(containerHtml);
@@ -69,13 +76,3 @@ $(".container").append(containerHtml);
 	$(this).text(cats[index].name);
 });*/
 
-$("img").click( function() {
-		for(let i = 0; i < cats.length; i++) {
-			if($(this).attr("data-cat") === cats[i].name) {
-				cats[i].numClicks++;
-				$(this).next().find("span").text(cats[i].numClicks);
-
-			}
-		}
-	}
-);
